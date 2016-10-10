2c77074bb7a4b53f612715a3900738f3ec5f9a40
cccikov <522978139@qq.com>
2016-10-10 14:44:00 +0800

    node

--- imooc/school/class.js
+++ imooc/school/class.js
@@ -1,6 +1,6 @@
 //3.class 模块 这个模块需要teacher模块还有student模块
-var student = require('./student.js'); //加载student模块 ./不能省略
-var teacher = require('./teacher.js');//加载teacher模块
+var student = require('./student.js'); //加载student模块 ./不能省略 要不就从根目录开始d:/learnNode/imooc/school/student
+var teacher = require('./teacher');//加载teacher模块 .js可以省略
 
 exports.add = function(teacherName,students){ // 可以用这种函数表达式写
 	teacher.add(teacherName);
