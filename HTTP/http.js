f7a3ddbfc49b6d4b61f4dceef2cd91a8a5ea5368
cccikov <522978139@qq.com>
2016-10-17 09:06:11 +0800

    http

--- HTTP/http.js
+++ HTTP/http.js
@@ -1,7 +1,35 @@
-var http require('http');
+// var http=require("http");
 
+// http.createServer(function(req,res){
+//     res.writeHead(200,{
+//         "content-type":"text/plain"
+//     });
+//     res.write("hello nodejs");
+//     res.end();
+// }).listen(3000);
+
+/*var http = require("http");
 http.createServer(function(req,res){
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.write('hello nodejs');
+    res.writeHead(200,{
+        "content-type":"text/plain"
+    });
+    res.write("hello nodejs2");
     res.end();
-}).listen(2015);
+
+}).listen(3000);
+*/
+
+/*上面代码可以改写成这样*/
+var http=require("http");
+var server=new http.Server();
+
+server.on("request",function(req,res){
+    res.writeHead(200,{
+        "content-type":"text/plain"
+    });
+    res.write("hello nodejs");
+    res.end();
+});
+server.listen(3000);
+
+// 其实createServer的回调函数 就是 request 的监听器 , createServer方法会返回http.server对象 , server的listen事件触发request事件
