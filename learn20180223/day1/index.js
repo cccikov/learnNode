d935744a1856eef6a0d9b528a4305f7107591a6e
ccc <522978139@qq.com>
2018-02-23 17:39:15 +0800

    node学习

---
+++ learn20180223/day1/index.js
@@ -0,0 +1,13 @@
+console.log("fuck");
+
+var http = require("http");
+var url = require("url");
+
+var server = http.createServer(function(req, res) {
+    var url = req.url; // 用户的请求url地址
+    console.log("访问的地址是" + url);
+    res.writeHeader(200, {
+        "content-type": "text/plain"
+    });
+    res.end("<h1>hello world</h1>");
+}).listen(3000);
