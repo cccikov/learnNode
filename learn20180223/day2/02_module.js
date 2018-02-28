600708f6ed9eb06247374917199f73423463452e
ccc <522978139@qq.com>
2018-02-28 17:52:02 +0800

    node module

--- learn20180223/day2/02_module.js
+++ learn20180223/day2/02_module.js
@@ -3,3 +3,6 @@ console.log(some_module);
 
 console.log(some_module.a)
 some_module.fn();
+
+var bluebird = require("bluebird"); // 没有 ./ ../ / 路径符 , 又不是node的核心模块 , 就是在 当前js文件的父目录的node_modules/ 里面找 , 如果还是没有找到，则移动到再上一层父目录，直到文件系统的根目录。
+console.log(bluebird);
