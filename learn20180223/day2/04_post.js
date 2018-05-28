dd975e9b0b1ec4840b9712ea2e4febcd71177c31
ccc <522978139@qq.com>
2018-05-28 14:02:19 +0800

    post 请求

--- learn20180223/day2/04_post.js
+++ learn20180223/day2/04_post.js
@@ -20,7 +20,9 @@ var server = http.createServer(function (req, res) {
             console.log(chunk.toString()); // 二进制转化为字符串
         });
         req.addListener("end", function () { // stream 的'end'事件
-            res.end("haha");
+            data = querystring.parse(data);
+            data = JSON.stringify(data);
+            res.end(data);
         });
     }
 
