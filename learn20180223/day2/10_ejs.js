5ecaea5140e57bb948bc3707b9495c82ea59e759
ccc <522978139@qq.com>
2018-05-29 20:32:42 +0800

    esj

--- learn20180223/day2/10_ejs.js
+++ learn20180223/day2/10_ejs.js
@@ -2,36 +2,120 @@
  * 用法1：
  * var template = ejs.compile(str, options);
  * template(data);
- * 
- * 
+ *
+ *
  * 用法2：
  * ejs.render(str, data, options);
  *
- * 
+ *
  * 用法3：
  * ejs.renderFile(filename, data, options, function(err, str){
  * });
  */
 
 var ejs = require("ejs");
+var fs = require('fs');
+var http = require('http');
+var static = require('./static');
 
-var str =
-`<% if (user) { %>
-    <h2><%= user.name %></h2>
-<% } %>`;
-var data = {
-    user: {
-        name: "ccc",
-        age: 18
+
+{
+    let str = `<% if (user) { %>
+            <h2><%= user.name %></h2>
+        <% } %>`;
+    let data = {
+        user: {
+            name: "ccc",
+            age: 18
+        }
+    };
+    let options = {};
+
+    // 用法1
+    let template = ejs.compile(str, options);
+    let html1 = template(data);
+    console.log(html1);
+
+    // 用法2
+    let html2 = ejs.render(str, data, options);
+    console.log(html2);
+}
+
+
+{
+
+    var tplData = {
+        list: [{
+            num: 0
+        }, {
+            num: 1
+        }, {
+            num: 2
+        }, {
+            num: 3
+        }, {
+            num: 4
+        }, {
+            num: 5
+        }, {
+            num: 6
+        }, {
+            num: 7
+        }]
+    };
+
+
+    // 方式1
+    fs.readFile(__dirname + "/10_ejs.ejs", function(err, data) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        var str = data.toString();
+
+        // 方式1 主要代码
+        var template = ejs.compile(str);
+        var html = template(tplData);
+
+        console.log(html);
+    });
+
+    // 方式2
+    fs.readFile(__dirname + "/10_ejs.ejs", function(err, data) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        var str = data.toString();
+
+        // 方式2 主要代码
+        var html = ejs.render(str, tplData);
+
+        console.log(html);
+    });
+
+
+    // 方式3，方式3是将fs都封装了
+    ejs.renderFile(__dirname + "/10_ejs.ejs", tplData, {}, function(err, str) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log(str);
+    });
+
+}
+
+
+// 创建服务器
+http.createServer(function(req, res) {
+    if (req.url == "/") {
+        ejs.renderFile(__dirname + "/10_ejs.ejs", tplData, {}, function(err, str) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        res.end(str);
+    });
     }
-};
-var options = {};
-
-// 用法1
-var template = ejs.compile(str, options);
-var html1 = template(data);
-console.log(html1);
-
-// 用法2
-var html2 = ejs.render(str, data, options);
-console.log(html2);
+}).listen(3000);
