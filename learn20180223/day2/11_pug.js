0f601a8aa41b02a41856ff58505d29162c5bae2b
ccc <522978139@qq.com>
2018-05-30 23:43:47 +0800

    pug

--- learn20180223/day2/11_pug.js
+++ learn20180223/day2/11_pug.js
@@ -12,28 +12,44 @@ var static = require('./static');
 
 
 /**
+ * 方法1
  * // compile
  * var fn = pug.compile('string of pug', options);
- * var html = fn(locals);
+ * var html = fn(data);
  *
+ *
+ * 方法2
  * // render
- * var html = pug.render('string of pug', merge(options, locals));
+ * var html = pug.render('string of pug', merge(options, data));
  *
+ * merge(options, data) 是指 选项option 和 数据data都写在这个对象
+ * merge 是 合并的意思
+ *
+ *
+ *
+ *
+ * 方法3
  * // renderFile
- * var html = pug.renderFile('filename.pug', merge(options, locals));
+ * var html = pug.renderFile('filename.pug', merge(options, data));
  */
+{
+    let template = pug.compile('title=title', {});
+    let html = template({
+        title: "方法1"
+    });
+    console.log(html);
+}
 
+{
+    let html = pug.render('title=title', {
+        title: "方法2"
+    });
+    console.log(html);
+}
 
-var html = pug.renderFile('./11_pug.pug');
-console.log(html);
-
-
-
-
-// 创建服务器
-http.createServer(function(req, res) {
-    if (req.url == "/") {
-        var html = pug.renderFile('./11_pug.pug') ;
-        res.end(html);
-    }
-}).listen(3000);
+{
+    let html = pug.renderFile('./11_pug.pug', {
+        title: "方法3"
+    });
+    console.log(html);
+}
