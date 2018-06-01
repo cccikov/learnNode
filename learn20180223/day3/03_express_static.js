32f7cbea3aed2aa085e9d094c1f5e10a6eb64fb7
ccc <522978139@qq.com>
2018-06-01 20:12:10 +0800

    总结

--- learn20180223/day3/03_express_static.js
+++ learn20180223/day3/03_express_static.js
@@ -5,9 +5,7 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static("public"),function(){
-
-});
+app.use(express.static("public"));
 // express.static为中间件
 // 中间件（Middleware） 是一个函数，它可以访问请求对象（request object (req)）, 响应对象（response object (res)）, 和 web 应用中处于请求-响应循环流程中的中间件，一般被命名为 next 的变量。
 
