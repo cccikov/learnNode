aede82aa818cbc565ea3cf8a6ebb9fbc56259af7
ccc <522978139@qq.com>
2018-06-01 17:53:14 +0800

    中间件

---
+++ learn20180223/day3/05_middleware.js
@@ -0,0 +1,24 @@
+// 中间件
+/* 中间件（Middleware） 是一个函数，它可以访问请求对象（request object (req)）, 响应对象（response object (res)）, 和 web 应用中处于请求-响应循环流程中的中间件，一般被命名为 next 的变量。 */
+var express = require("express");
+var app = express();
+
+// 没有挂载路径的中间件，应用的每个请求都会执行该中间件
+app.use(function (req, res, next) {
+    console.log('Time:', Date.now());
+    next();
+});
+
+// 挂载至 /user/:id 的中间件，任何指向 /user/:id 的请求都会执行它
+app.use('/user/:id', function (req, res, next) {
+    console.log('Request Type:', req.method);
+    next();
+});
+
+// 路由和句柄函数(中间件系统)，处理指向 /user/:id 的 GET 请求
+app.get('/user/:id', function (req, res, next) {
+    res.send('USER');
+});
+
+app.listen(3000);
+
---
+++ learn20180223/day3/06_middleware2.js
@@ -0,0 +1,45 @@
+/**
+ * 中间件流程顺序
+ */
+var express = require("express");
+
+
+/* 采用顺序1 */
+{
+    let app = express();
+
+    // 假如访问的页面网址 "/用户名/用户id"格式的 如： "http://localhost:3000/ccc/123456"
+    app.get('/:username/:id', function (req, res, next) {
+        console.log(req.params);
+        res.send("用户名：" + req.params.username + "，id：" + req.params.id);
+    });
+
+    // 假如再有一个页面是 后面管理登录页面 /admin/login
+    app.get('/admin/login', function (req, res, next) {
+        res.send("后台管理登录页面")
+    });
+
+    app.listen(3000);
+
+    // 当访问 "http://localhost:3000/admin/login"  页面会显示 用户名：admin，id：login
+    // 因为/admin/login也是符合第一个路由的格式，第一个路由已经终止了请求-响应循环，这个请求就已经结局了，不会再调用下一个中间件，除非调用next()
+}
+
+
+
+
+{
+
+    /* 更改了一下 中间件流程顺序 ， 注意这个端口是8000 */
+    let app = express();
+    app.get('/admin/login', function (req, res, next) {
+        res.send("后台管理登录页面")
+    });
+    app.get('/:username/:id', function (req, res, next) {
+        console.log(req.params);
+        res.send("用户名：" + req.params.username + "，id：" + req.params.id);
+    });
+    app.listen(8000);
+
+    // "http://localhost:8000/admin/login" 现在页面显示的是 后台管理登录页面
+}
