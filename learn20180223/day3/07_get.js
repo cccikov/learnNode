a1a652de96e611f890596e1dae29c31d05aeabf8
ccc <522978139@qq.com>
2018-06-07 15:50:37 +0800

    get

--- learn20180223/day3/07_get.js
+++ learn20180223/day3/07_get.js
@@ -1,7 +1,14 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static("./"));
+app.use(express.static("./",{
+    index:"07_get.html"
+}));
 
+app.get("/me/getdata",function(req,res){
+    // 不需要再通过url模块去分析参数部分，直接用req.query
+    console.log(req.query);
+    res.send(req.query);
+});
 
 app.listen(3000);
