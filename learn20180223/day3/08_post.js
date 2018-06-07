71fd3476d9cc14ef0a6102584331bc327326726c
ccc <522978139@qq.com>
2018-06-07 23:50:32 +0800

    post

--- learn20180223/day3/08_post.js
+++ learn20180223/day3/08_post.js
@@ -1,14 +1,29 @@
 var express = require("express");
 var app = express();
+var bodyParser = require('body-parser');
 
-app.use(express.static("./",{
-    index:"08_post.html"
+app.use(express.static("./", {
+    index: "08_post.html"
 }));
 
-app.post("/me/postdata",function(req,res){
-    // 不需要再通过url模块去分析参数部分，直接用req.query
-    console.log(req.query);
-    res.send(req.query);
+/* app.use(bodyParser.json()); // for parsing application/json 但是好像angular的post请求是这个
+app.use(bodyParser.urlencoded({
+    extended: true
+})); // for parsing application/x-www-form-urlencoded post请求默认是"Content-Type":"application/x-www-form-urlencoded"
+
+app.post("/me/postdata", function (req, res) {
+    console.log(req.body);
+    res.send(req.body);
+}); */
+
+// 相当于
+
+app.post("/me/postdata", bodyParser.urlencoded({
+    extended: true
+}), bodyParser.json(), function (req, res) {
+    console.log(req.body);
+    res.send(req.body);
 });
+// 这里也是3个中间件，也是要考虑顺序的
 
 app.listen(3000);
