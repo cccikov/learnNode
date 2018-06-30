0ee6469b14057c95995001dc295f580929d53822
ccc_ubuntu <522978139@qq.com>
2018-06-30 23:17:36 +0800

    req.ip

--- learn20180223/day3/10_req.js
+++ learn20180223/day3/10_req.js
@@ -48,6 +48,6 @@ app.get("/index/admin", (req, res, next) => {
 app.listen(3000);
 //  http://localhost:3000/index/admin
 
-
+// http://127.0.0.1:3000/index/admin ip 会显示为 ::ffff:127.0.0.1
 // windows从Windows 7以后默认启用IPv6。即使我的服务器仅在IPv4上侦听，Windows 7也会将::ffff:前缀发送到IPv4，作为向IPv6过渡的一部分
 // 它是IPv4地址的IPv6版本。
