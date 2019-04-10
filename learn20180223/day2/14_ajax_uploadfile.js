ad1355626d00f16aa76fb34948472bc0a6bd7c47
ccc <522978139@qq.com>
2019-04-10 11:38:09 +0800

    可以上传多个图片

--- learn20180223/day2/14_ajax_uploadfile.js
+++ learn20180223/day2/14_ajax_uploadfile.js
@@ -1,38 +1,40 @@
 var formidable = require('formidable'),
     http = require('http'),
-    util = require('util');
+    util = require('util'),
+    path = require('path');
 var static = require("./static");
 var url = require("url");
 
 http.createServer(function (req, res) {
     var urlObj = url.parse(req.url);
     var pathname = urlObj.pathname;
-    console.log("请求地址： "+pathname);
+    console.log("请求地址： " + pathname);
     if (pathname == '/upload' && req.method.toLowerCase() == 'post') {
         console.log("进来了")
         // parse a file upload
         var form = new formidable.IncomingForm();
         form.uploadDir = "./";
+        form.multiples = true; // 多文件上传
 
         form.parse(req, function (err, fields, files) {
             console.log(err, fields, files)
             res.writeHead(200, {
                 'content-type': 'text/plain'
             });
-            res.write('received upload:\n\n');
-            res.end(util.inspect({
+            res.write(util.inspect({
                 fields: fields,
                 files: files
-            }));
+            }, false, 3)); // 递归3层
+            res.end();
         });
 
-    }  else if (pathname == "/axios.min.js") { // 路由控制，axios.min.js
-        static(req, res, "../public/axios.min.js");
+    } else if (pathname == "/axios.min.js") { // 路由控制，axios.min.js
+        static(req, res, path.resolve(__dirname, "../public/axios.min.js"));
     } else if (pathname == "/axios.min.map") { // 路由控制，axios.min.map 返回空
         res.end("")
-    }else {
+    } else {
         if (req.url == "/") { // "/" 访问 默认的页面
-            static(req, res, "./14_ajax_uploadfile.html");
+            static(req, res, path.resolve(__dirname, "14_ajax_uploadfile.html"));
             return;
         }
         static(req, res);
