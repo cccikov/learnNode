fa95ce60ce2ba507f7ce11260f53a4e95f1b8c4d
ccc <522978139@qq.com>
2020-04-03 14:35:49 +0800

    关闭 express 服务

---
+++ learn20180223/day3/01_express_b.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var http = require("http")
+
+var app = express();
+var server = http.createServer(app); // / 返回一个 http.Server 实例，接收的参数，就是类似express中间件，所以可以使用将app作为参数传入
+
+app.get("/", function (req, res) {
+    res.send("hello world");
+});
+
+app.get("/close", function (req, res) {
+    console.log("服务准备关闭")
+    server.close(_ => {
+        console.log("服务成功关闭")
+    });
+    res.send("服务即将关闭");
+});
+
+server.listen(3000);
