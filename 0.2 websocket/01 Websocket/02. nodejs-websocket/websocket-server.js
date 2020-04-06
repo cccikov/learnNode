5a2e3831f9e26f2acec8db21df74822cdb21288f
ccc <522978139@qq.com>
2020-04-06 17:11:28 +0800

    node package：nodejs-websocket 简单demo，html5原生websocket

---
+++ 0.2 websocket/01 Websocket/02. nodejs-websocket/websocket-server.js
@@ -0,0 +1,21 @@
+var ws = require("nodejs-websocket")
+
+// Scream server example: "hi" -> "HI!!!"
+var server = ws.createServer(function (conn) {
+
+    console.log("New connection")
+
+    conn.on("text", function (str) {
+        console.log("Received " + str)
+        conn.sendText(str.toUpperCase() + "!!!")
+    })
+
+    conn.on("error", function (arg) {
+        console.log(arg)
+    })
+
+    conn.on("close", function (code, reason) {
+        console.log("Connection closed")
+    })
+
+}).listen(8080)
